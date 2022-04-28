0f61bc74ba9a33ab0cc8828053d091c4decf3e62
w112749 <w112749@WL-30XP7C3>
2022-04-28 12:55:57 +0530

    Added Dashboard Navigation

--- PlanView/src/Practice.java
+++ PlanView/src/Practice.java
@@ -20,39 +20,58 @@ import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.sikuli.script.FindFailed;
-import org.sikuli.script.Pattern;
-
-import org.sikuli.script.Screen;
-
-
-import com.gargoylesoftware.htmlunit.WebWindow;
-import com.gargoylesoftware.htmlunit.javascript.background.JavaScriptExecutor;
 
 public class Practice {
 
 	static {
 		System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\a566317\\OneDrive - Worldline\\Documents\\My Received Files\\Demo\\Browser\\chromedriver.exe");
+				"C:\\Users\\w112749\\Downloads\\chrome_driver1\\chromedriver.exe");
 		System.out.println("Static block");
 	}
 
-	public static void main(String[] args) throws InterruptedException, AWTException, FindFailed, IOException {
+	public static void main(String[] args) throws InterruptedException, AWTException, IOException {
 
-		WebDriver driver = new ChromeDriver ();
+WebDriver driver = new ChromeDriver ();
+		
+		
+		driver.get("https://worldline.pvcloud.com/");
+		
+		Thread.sleep(5000L);
 		
 		driver.manage().window().maximize();
-		driver.get("https://one.myworldline.com/en/home.html");
+		Thread.sleep(2000L);
+
+		WebElement clickLoginButton = driver.findElement(By.xpath("//div['@class=\"wg-pki\"']//input[4]"));
+				clickLoginButton.click();
 		
-		Runtime.getRuntime().exec("C:\\Users\\a566317\\eclipse-workspace\\CodeMaster_sikuli.jar");
-		Thread.sleep(30000);
-		Thread.sleep(5000);
-		System.out.println("Title of the page: " + driver.getTitle());
-
-		//driver.findElement(By.xpath("//a[@href='javascript: document.logoutForm.submit()']")).click();
-
-		Thread.sleep(5000);
-		driver.quit();
+		//C:\\Users\\w112749\\OneDrive - Worldline\\Documents\\test.exe
+		
+		//Runtime.getRuntime().exec("C:\\Users\\w112749\\OneDrive - Worldline\\Documents\\test.exe");
+		Thread.sleep(10000L);
+		System.out.println(driver.getTitle());
+		
+		Thread.sleep(2000L);
+		WebElement searchClick=driver.findElement(By.id("bannerSearchBox"));
+		WebDriverWait wait= new WebDriverWait(driver, 5000);
+		
+		searchClick.sendKeys("marc");
+		Thread.sleep(5000L);
+		
+		String textToSelect = "WL BE FS TCC Marco Polo";
+		
+		WebElement optionsToSelect = driver.findElement(By.xpath("//ul[@id='searchUl']/li/a[@title='WL BE FS TCC Marco Polo']"));
+		optionsToSelect.click();
+		
+		driver.getTitle();
+		Thread.sleep(5000L);
+		
+		WebElement test1=driver.findElement(By.xpath("//button[@id='PVBannerTitleBarMenuButton']/span[@title='Actions']"));
+		test1.click();
+		
+		Thread.sleep(3000L);
+		WebElement test2=driver.findElement(By.xpath("//span[@class='bannerMenuItemText'][contains(text(),'Work and Assignments')]"));
+		test2.click();
+		Thread.sleep(3000L);
 
 	}
 
