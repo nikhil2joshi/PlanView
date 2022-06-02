c55ef918f37acbb5bd76d7d749d7cf10d7bf26ba
nikhil2joshi <nikhil.2.joshi@worldline.com>
2022-06-02 17:33:04 +0530

    Added Window Builder GUI

--- PlanView/src/testCases/PlanViewBaseClass.java
+++ PlanView/src/testCases/PlanViewBaseClass.java
@@ -1,132 +1,307 @@
 package testCases;
 
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.Label;
+import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
 
+import java.sql.Connection;
+
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
-import java.awt.AWTException;
-import java.io.IOException;
-import org.openqa.selenium.By;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.SwingUtilities;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import testCases.AddAllocation;
+import testCases.ExcelDataObject;
+import testCases.NewRequirement;
+import testCases.SearchProject;
+import testCases.WorkAssignmentPage;
+
+//import javax.swing.JRadioButton;
+
 public class PlanViewBaseClass {
 	public static List<ExcelDataObject> excelDataObjects;
+	public static ChromeOptions options;
 	static {
 
 		String path = System.getProperty("user.dir");
 		System.out.println("Chromedriver loading" + path);
 		System.setProperty("webdriver.chrome.driver", path + "\\chromedriver.exe");
-
+		options = new ChromeOptions();
+		options.addArguments("--headless");
+		ChromeDriver chromeDriver = new ChromeDriver(options);
 		System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
 
 	}
 
-	public static void main(String[] args) throws InterruptedException, AWTException, IOException {
-		WebDriver driver = new ChromeDriver();
-		try {
+	private JFrame frame;
+	private static JButton btnNewButton;
 
-			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-			WebDriverWait wait = new WebDriverWait(driver, 10000);
-			// driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-			driver.get("https://worldline.pvcloud.com/");
-			driver.manage().window().maximize();
+	/*
+	 * Export excel file location
+	 */
 
-			driver.findElement(By.xpath("//div[@class='wg-pki']//input[4]")).click(); // click on login Button
+	static String head;
+	static String line;
+	static List<String> amexDetails = new ArrayList<String>();
+	static int numberOfColumns = 4;
+	static Label lbl1;
+	static Iterator<String> itr1;
 
-			Thread.sleep(10000L);// Wait for user to select certificate and enter PIN
+	/*
+	 * DB Connection Details
+	 */
 
-			System.out.println(driver.getTitle());
+	private JTabbedPane tabbedPane;
 
-			Actions action1 = new Actions(driver);
+	private static JTextArea textArea_Console;
 
-			// workAssignmentPage.createTask(driver, wait, action1);
+	private static Connection conn;
 
-			ExcelDataObject excelDataObject = new ExcelDataObject();
-			List<ExcelDataObject> excelDataObjects2 = excelDataObject.getExcelData(
-					System.getProperty("user.dir") + "\\TimesheetTasksCollection.xlsm",
-					excelDataObjects);
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
 
-			Iterator<ExcelDataObject> iterator = excelDataObjects2.iterator();
-			// First time add requirement and allocate
-			String currentTaskName = null;
+					PlanViewBaseClass window = new PlanViewBaseClass();
+					window.frame.setVisible(true);
+					window.frame.setExtendedState(java.awt.Frame.MAXIMIZED_BOTH);
+					textArea_Console.setText(null);
+					btnNewButton.addActionListener(new ActionListener() {
+						public void actionPerformed(ActionEvent e) {
+							
+							WebDriver driver = new ChromeDriver(options);
+							try {
+								redirectSystemStreams(textArea_Console);
+								
+								driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+								WebDriverWait wait = new WebDriverWait(driver, 10000);
+								// driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+								driver.get("https://worldline.pvcloud.com/");
+								driver.manage().window().maximize();
 
-			NewRequirement newRequirement;
-			AddAllocation addAllocation;
-			WorkAssignmentPage workAssignmentPage = new WorkAssignmentPage();
-			boolean flag = false;
-			excelDataObject = (ExcelDataObject) iterator.next();
-			if (iterator.hasNext()) {
+								driver.findElement(By.xpath("//div[@class='wg-pki']//input[4]")).click(); // click on
+																											// login
+																											// Button
 
-				if (excelDataObject.empName == null) {
-					SearchProject obj1 = new SearchProject();
-					obj1.searchbyprojectname(driver, wait, excelDataObject.projectName);
+								Thread.sleep(10000L);// Wait for user to select certificate and enter PIN
 
-					workAssignmentPage.navigateWorkAssignmentPage(driver, wait);
-					excelDataObject = (ExcelDataObject) iterator.next();
-					currentTaskName = excelDataObject.taskName;
+								System.out.println(driver.getTitle());
+
+								Actions action1 = new Actions(driver);
+
+								// workAssignmentPage.createTask(driver, wait, action1);
+
+								ExcelDataObject excelDataObject = new ExcelDataObject();
+								List<ExcelDataObject> excelDataObjects2 = excelDataObject.getExcelData(
+										System.getProperty("user.dir") + "\\TimesheetTasksCollection.xlsm",
+										excelDataObjects);
+
+								Iterator<ExcelDataObject> iterator = excelDataObjects2.iterator();
+								// First time add requirement and allocate
+								String currentTaskName = null;
+
+								NewRequirement newRequirement;
+								AddAllocation addAllocation;
+								WorkAssignmentPage workAssignmentPage = new WorkAssignmentPage();
+								boolean flag = false;
+								excelDataObject = (ExcelDataObject) iterator.next();
+								if (iterator.hasNext()) {
+
+									if (excelDataObject.empName == null) {
+										SearchProject obj1 = new SearchProject();
+										obj1.searchbyprojectname(driver, wait, excelDataObject.projectName);
+
+										workAssignmentPage.navigateWorkAssignmentPage(driver, wait);
+										excelDataObject = (ExcelDataObject) iterator.next();
+										currentTaskName = excelDataObject.taskName;
+									}
+
+								}
+								while (iterator.hasNext() && excelDataObject.empName != null) {
+
+									// first task addition without comparison
+									workAssignmentPage.createTask(driver, wait, action1, excelDataObject);
+
+									newRequirement = new NewRequirement();
+									newRequirement.addNewRequirement(driver, action1, excelDataObject.gcmRole);
+
+									addAllocation = new AddAllocation();
+									addAllocation.addAllocation(driver, action1, excelDataObject);
+
+									if (iterator.hasNext())
+										excelDataObject = (ExcelDataObject) iterator.next();
+
+									// Compare next task in loop with earlier task if same then only add GCM and
+									// Employee
+									// If not then come out of for loop and add new task
+									for (; iterator.hasNext() && currentTaskName.equals(excelDataObject.taskName);) {
+
+										newRequirement = new NewRequirement();
+										newRequirement.addNewRequirement(driver, action1, excelDataObject.gcmRole);
+
+										addAllocation = new AddAllocation();
+										addAllocation.addAllocation(driver, action1, excelDataObject);
+										excelDataObject = (ExcelDataObject) iterator.next();
+
+									}
+									if (iterator.hasNext())
+										currentTaskName = excelDataObject.taskName;
+								}
+								// adding last entry
+								if (!iterator.hasNext()) {
+									if (currentTaskName.equals(excelDataObject.taskName)) {
+										newRequirement = new NewRequirement();
+										newRequirement.addNewRequirement(driver, action1, excelDataObject.gcmRole);
+
+										addAllocation = new AddAllocation();
+										addAllocation.addAllocation(driver, action1, excelDataObject);
+
+									} else {
+										workAssignmentPage.createTask(driver, wait, action1, excelDataObject);
+
+										newRequirement = new NewRequirement();
+										newRequirement.addNewRequirement(driver, action1, excelDataObject.gcmRole);
+
+										addAllocation = new AddAllocation();
+										addAllocation.addAllocation(driver, action1, excelDataObject);
+
+									}
+								}
+
+							} catch (Exception e1) {
+								e1.printStackTrace();
+							} finally {
+								driver.quit();
+							}
+
+						}
+					});
+
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
 
 			}
-			while (iterator.hasNext() && excelDataObject.empName != null) {
+		});
+	}
 
-				// first task addition without comparison
-				workAssignmentPage.createTask(driver, wait, action1, excelDataObject);
+	/**
+	 * Create the application.
+	 */
+	public PlanViewBaseClass() {
+		initialize();
+	}
 
-				newRequirement = new NewRequirement();
-				newRequirement.addNewRequirement(driver, action1, excelDataObject.gcmRole);
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
 
-				addAllocation = new AddAllocation();
-				addAllocation.addAllocation(driver, action1, excelDataObject);
+		frame = new JFrame();
+		frame.setBounds(100, 100, 536, 468);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-				if (iterator.hasNext())
-					excelDataObject = (ExcelDataObject) iterator.next();
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[] { 0, 0, 0, 400, 0, 0, 0, 0, 0 };
+		gbl_panel.rowHeights = new int[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+		gbl_panel.columnWeights = new double[] { 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0, Double.MIN_VALUE };
+		gbl_panel.rowWeights = new double[] { 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
+		panel.setLayout(gbl_panel);
 
-				// Compare next task in loop with earlier task if same then only add GCM and
-				// Employee
-				// If not then come out of for loop and add new task
-				for (; iterator.hasNext() && currentTaskName.equals(excelDataObject.taskName);) {
+		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		GridBagConstraints gbc_tabbedPane = new GridBagConstraints();
+		gbc_tabbedPane.gridwidth = 4;
+		gbc_tabbedPane.insets = new Insets(0, 0, 5, 5);
+		gbc_tabbedPane.fill = GridBagConstraints.BOTH;
+		gbc_tabbedPane.gridx = 0;
+		gbc_tabbedPane.gridy = 0;
+		panel.add(tabbedPane, gbc_tabbedPane);
 
-					newRequirement = new NewRequirement();
-					newRequirement.addNewRequirement(driver, action1, excelDataObject.gcmRole);
+		JPanel panel_2 = new JPanel();
+		tabbedPane.addTab("PlanView", null, panel_2, null);
+		panel_2.setLayout(null);
 
-					addAllocation = new AddAllocation();
-					addAllocation.addAllocation(driver, action1, excelDataObject);
-					excelDataObject = (ExcelDataObject) iterator.next();
+		textArea_Console = new JTextArea();
+		textArea_Console.setBounds(282, 50, 641, 560);
+		panel_2.add(textArea_Console);
 
-				}
-				if (iterator.hasNext())
-					currentTaskName = excelDataObject.taskName;
+		JScrollPane scrollPane = new JScrollPane(textArea_Console);
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollPane.setBounds(24, 100, 941, 562);
+		panel_2.add(scrollPane);
+
+		btnNewButton = new JButton("Start");
+		btnNewButton.setBounds(189, 39, 85, 21);
+		panel_2.add(btnNewButton);
+
+	}
+
+
+	private static void updateTextArea(final String text, final JTextArea Console) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				Console.append(text);
+
 			}
-			//adding last entry
-			if (!iterator.hasNext()) {
-				if (currentTaskName.equals(excelDataObject.taskName)) {
-					newRequirement = new NewRequirement();
-					newRequirement.addNewRequirement(driver, action1, excelDataObject.gcmRole);
-					
-					addAllocation = new AddAllocation();
-					addAllocation.addAllocation(driver, action1, excelDataObject);
+		});
+	}
 
-				} else {
-					workAssignmentPage.createTask(driver, wait, action1, excelDataObject);
-
-					newRequirement = new NewRequirement();
-					newRequirement.addNewRequirement(driver, action1, excelDataObject.gcmRole);
-
-					addAllocation = new AddAllocation();
-					addAllocation.addAllocation(driver, action1, excelDataObject);
-					
-				}
+	private static void redirectSystemStreams(final JTextArea Console) {
+		OutputStream out = new OutputStream() {
+			@Override
+			public void write(int b) throws IOException {
+				updateTextArea(String.valueOf((char) b), Console);
 			}
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			driver.quit();
-		}
+			@Override
+			public void write(byte[] b, int off, int len) throws IOException {
+				updateTextArea(new String(b, off, len), Console);
+			}
+
+			@Override
+			public void write(byte[] b) throws IOException {
+				write(b, 0, b.length);
+			}
+		};
+
+		System.setOut(new PrintStream(out, true));
+		System.setErr(new PrintStream(out, true));
 	}
 }
